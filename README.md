5c224fc96af2297a66916bc6d1388dbe2a3f0192
Himanshu Joshi <himanshu.joshi@digitaltrons.com>
2021-09-25 17:08:33 +0530

    remove files from blog-web/src and create app, index own

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # micros-with-rect-nodejs
+
+# Post Feature
+-  API for create post. 
+   -  **User story**: User when enter title in field and hit submit button this API will create post in database.
+   -  **Method**: POST
+   -  **Body**: {title: string}
+-  API for list all posts. Method will be GET.
---
+++ comments/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const {randomBytes } = require('crypto');
+const app = express();
+
+app.use(express.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.status(201).send(commentsByPostId[req.params.id]|| []);
+ });
+
+app.post("/posts/:id/comments", (req, res) => {
+    const id = randomBytes(5).toString("hex");
+    const { content } = req.body;
+
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({ id, content });
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(201).send(comments);
+});
+
+app.listen(4001, () => { 
+    console.log('Listening on port 4001 ...');
+})
